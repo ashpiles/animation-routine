5dd3f819c181327da1915878fd71db67ea01dba4
Mia <miasedgewick@gmail.com>
2025-05-04 21:33:42 -0700

    Intergated FRP Behaviors

--- Source/AnimationRoutineTool/AnimationRoutine.cpp
+++ Source/AnimationRoutineTool/AnimationRoutine.cpp
@@ -1,47 +1,50 @@
-#include "AnimationRoutine.h"
-#include "AnimationTask.h"
-#include "BoneRecorder.h"
 #include "Animation/AnimCurveTypes.h"
-#include "Animation/AnimSequence.h"
-#include "Animation/AnimData/IAnimationDataModel.h"
 #include "Animation/AnimData/AnimDataModel.h"
+#include "Animation/AnimData/IAnimationDataModel.h"
+#include "Animation/AnimSequence.h"
+#include "AnimationRoutine.h"
 #include "AssetViewUtils.h"
 
-
-void UAnimRoutine::MapTaskToAnim(const UAnimSequence* Anim, const UAnimTask* Task)
+void UAnimRoutine::MapTaskToAnim(UAnimSequence* Anim, FName BoneName)
 {
-	TArray<FName> BoneTrackNames {};
-	TArray<FTransform> OutTransforms {};
-	Anim->GetDataModel()->GetBoneTrackNames(BoneTrackNames);
-	// Test First Apply
-	Task->ApplyTaskTo(Anim, BoneTrackNames[0], OutTransforms);
-
-	for(FTransform& Transform : OutTransforms)
+	if (Anim)
 	{
-		UE_LOG(LogTemp, Display, TEXT("%s Transform:\n%s"), *BoneTrackNames[0].ToString(), *Transform.ToString());
-	}
-	// Test Second Apply
-	// Test Third Apply
-	// Test Fourth Apply
-	
+		TArray<FName> BoneTrackNames{};
+		Anim->GetDataModel()->GetBoneTrackNames(BoneTrackNames);
+		double AnimLength = Anim->GetDataModel()->GetPlayLength();
 
+		TFunction<double(double, double)> GetFrameTime = [&AnimLength](double frame, double time) {
+			time = (time / frame);
+			return FGenericPlatformMath::IsNaN(time) || !FGenericPlatformMath::IsFinite(time) ? 0 : AnimLength - time;
+		};
+
+		TFunction<double(double, double)> Bounce = [](double value, double time) {
+			return value * sin(time);
+		};
+
+		TFunction<bool(const FVector3f& Pos, const FQuat4f& Rot, const FVector3f Size, const FFrameNumber& Frame)> Iterator;
+		Iterator = [&AnimLength, &Bounce, &BoneName, &GetFrameTime](const FVector3f& Pos, const FQuat4f& Rot, const FVector3f Size, const FFrameNumber& Frame) -> bool {
+				double PosY = Bounce(Pos.Y, GetFrameTime(Frame.Value, AnimLength));
+				UE_LOG(LogTemp, Display, TEXT("%s Pos Y: %f"), *BoneName.ToString(), PosY);
+				return true;
+			};
+
+		Anim->GetDataModel()->IterateBoneKeys(BoneName, Iterator);
+	}
 }
 
 void UAnimRoutine::LoadAnimSequence(const FString& FilePath)
 {
-	UAnimSequence* Anim {LoadObject<UAnimSequence>(nullptr, *FilePath)};
-	if(Anim)
+	UAnimSequence* Anim{ LoadObject<UAnimSequence>(nullptr, *FilePath) };
+	if (Anim)
 	{
-		SourceAnim = Anim;
 	}
 }
 
-void UAnimRoutine::AddKey(const FString& AnimFilePath, float Time, const FName& BoneName, const FTransform& AdditiveTransform)
+void UAnimRoutine::AddKey(UAnimSequence* const Anim, float Time, const FName& BoneName, const FTransform& AdditiveTransform)
 {
-	UAnimSequence* Anim {LoadObject<UAnimSequence>(nullptr, *AnimFilePath)};
-	if(Anim)
+	if (Anim)
 	{
-		Anim -> AddKeyToSequence(Time, BoneName, AdditiveTransform);
+		Anim->AddKeyToSequence(Time, BoneName, AdditiveTransform);
 	}
-
 }
--- Source/AnimationRoutineTool/AnimationRoutine.h
+++ Source/AnimationRoutineTool/AnimationRoutine.h
@@ -18,12 +18,9 @@ public:
     void LoadAnimSequence(const FString& FilePath);
 
     UFUNCTION(BlueprintCallable, Category="MyEditorTools")
-     void AddKey(const FString& AnimFilePath, float Time, const FName& BoneName, const FTransform& AdditiveTransform);
+     void AddKey(UAnimSequence* const Anim, float Time, const FName& BoneName, const FTransform& AdditiveTransform);
 
     UFUNCTION(BlueprintCallable, Category="MyEditorTools")
-    static void MapTaskToAnim(const UAnimSequence* Anim, const UAnimTask* AnimTask);
-
-    UFUNCTION(BlueprintCallable, Category="MyEditorTools")
-    static void CreateTask();
+    static void MapTaskToAnim(UAnimSequence* Anim, FName BoneName);
 
 };
